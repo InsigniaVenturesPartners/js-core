71b94773fc87b3d52e308f4dc140130e97662f37
Phuoc Nguyen Thanh <phuocnt0612@gmail.com>
2020-05-11 18:04:06 +0700

    Add match regex

--- rafael-data/helpers/functions/index.js
+++ rafael-data/helpers/functions/index.js
@@ -2,10 +2,12 @@ import getIPAddress from './getIPAddress'
 import sleep from './sleep'
 import sleepRandomly from './sleepRandomly'
 import encodeRFC5987ValueChars from './encodeRFC5987ValueChars'
+import matchRegex from './matchRegex'
 
 export {
   getIPAddress,
   sleep,
   sleepRandomly,
-  encodeRFC5987ValueChars
+  encodeRFC5987ValueChars,
+  matchRegex
 }
