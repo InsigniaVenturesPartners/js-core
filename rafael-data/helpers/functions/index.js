5e4e09cdb89263d6de353036c33653b036daefb3
Jimmy Setiawan <jimmysetiawan.js@gmail.com>
2020-11-02 18:12:36 +0700

    add getLinkedinUsername

--- rafael-data/helpers/functions/index.js
+++ rafael-data/helpers/functions/index.js
@@ -3,11 +3,13 @@ import sleep from './sleep'
 import sleepRandomly from './sleepRandomly'
 import encodeRFC5987ValueChars from './encodeRFC5987ValueChars'
 import matchRegex from './matchRegex'
+import getLinkedinUsername from './getLinkedinUsername'
 
 export {
   getIPAddress,
   sleep,
   sleepRandomly,
   encodeRFC5987ValueChars,
-  matchRegex
+  matchRegex,
+  getLinkedinUsername
 }
