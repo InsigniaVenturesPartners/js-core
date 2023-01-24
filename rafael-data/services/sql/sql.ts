23559bcfb2410d26fba11917a06cae8b63236dd7
Indra <indra@insignia.vc>
2023-01-24 16:13:13 +0700

    SQL v2.0.0

--- rafael-data/services/sql/sql.ts
+++ rafael-data/services/sql/sql.ts
@@ -1,45 +1,69 @@
-import { Pool } from 'pg'
-import { logDanger, logInfo, logMagenta } from '@insigniateam/helpers.print-log'
-import Secret from "@insigniateam/rafael-data.secret"
+import { Pool } from "pg";
+import {
+  logDanger,
+  logInfo,
+  logMagenta,
+} from "@insigniateam/helpers.print-log";
+import Secret from "@insigniateam/rafael-data.secret";
+
+interface DbCredentials {
+  database: string;
+  host: string;
+  user: string;
+  password: string;
+  port: string;
+}
+let DB_CREDENTIALS: DbCredentials | undefined;
 
 export default class Sql {
-  static async connectToDb () {
-    const isDev = process.env.STAGE === 'dev'
-    const secret = await new Secret()
-
-    return new Pool({
-      database: process.env.PSQL_DB,
-      host: isDev ? process.env.PSQL_HOST : secret.data.db.host,
-      user: isDev ? process.env.PSQL_USER : secret.data.db.username,
-      password: isDev ? process.env.PSQL_PWD : secret.data.db.password,
-      port: isDev ? process.env.PSQL_PORT : secret.data.db.port
-    })
+  static async setDbCredentials(credentials: DbCredentials) {
+    DB_CREDENTIALS = credentials;
   }
 
-  static async execute (query, params = []) {
-    const db = await this.connectToDb()
+  static async connectToDb() {
+    const isDev = process.env.STAGE === "dev";
+    const secret = await new Secret();
+    if (!DB_CREDENTIALS) {
+      throw new Error("DB_CREDENTIALS not set");
+    }
+    return new Pool({
+      database: DB_CREDENTIALS.database,
+      host: isDev ? DB_CREDENTIALS.host : secret.data.db.host,
+      user: isDev ? DB_CREDENTIALS.user : secret.data.db.username,
+      password: isDev ? DB_CREDENTIALS.password : secret.data.db.password,
+      port: isDev ? DB_CREDENTIALS.port : secret.data.db.port,
+    });
+  }
 
-    params = Array.isArray(params) ? params : [params]
+  static async execute(query, params = []) {
+    const db = await this.connectToDb();
 
-    return db.query(query, params).then(value => {
-      db.end()
-      if (process.env.DB_DEBUG === 'true') {
-        logInfo('Running Query:')
-        logMagenta(query)
-        logMagenta(`Params: ${JSON.stringify(params)}`)
-        logInfo(`${value.rows.length} rows returned.`)
-      }
-      return value.rows
-    }).catch((err) => {
-      db.end()
+    params = Array.isArray(params) ? params : [params];
 
-      logDanger('Error When Running Query:')
-      logMagenta(query)
-      logMagenta(`Params: ${JSON.stringify(params)}`)
+    return db
+      .query(query, params)
+      .then((value) => {
+        db.end();
+        if (process.env.DB_DEBUG === "true") {
+          logInfo("Running Query:");
+          logMagenta(query);
+          logMagenta(`Params: ${JSON.stringify(params)}`);
+          logInfo(`${value.rows.length} rows returned.`);
+        }
+        return value.rows;
+      })
+      .catch((err) => {
+        db.end();
 
-      const errMsg = `[SQL Error] ${err.message}. Query: ${query}. Params: ${JSON.stringify(params)}`
-      logDanger(errMsg)
-      throw new Error(errMsg)
-    })
+        logDanger("Error When Running Query:");
+        logMagenta(query);
+        logMagenta(`Params: ${JSON.stringify(params)}`);
+
+        const errMsg = `[SQL Error] ${
+          err.message
+        }. Query: ${query}. Params: ${JSON.stringify(params)}`;
+        logDanger(errMsg);
+        throw new Error(errMsg);
+      });
   }
 }
