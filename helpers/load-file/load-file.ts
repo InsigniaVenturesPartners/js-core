d1d67facfa68cab57c2944ba4df156d5cbb0fc55
Indra <indra@insignia.vc>
2023-07-21 22:21:47 +0700

    Update load-file.ts

--- helpers/load-file/load-file.ts
+++ helpers/load-file/load-file.ts
@@ -1,22 +1,32 @@
-import * as fs from 'fs'
-import axios from 'axios'
+import * as fs from "fs";
+import axios, { ResponseType } from "axios";
 
-export async function loadFile (fileUrl, local = false) {
+export async function loadFile(fileUrl, local = false, axiosOptions = {}) {
   return new Promise(async (resolve, reject) => {
     if (local) {
       fs.readFile(fileUrl, (err, buffer) => {
-        if (err) reject(err)
+        if (err) reject(err);
 
-        resolve(buffer)
-      })
+        resolve(buffer);
+      });
     } else {
       try {
-        const response = await axios.get(fileUrl, { responseType: 'arraybuffer' })
-        const fileBuffer = Buffer.from(response.data, 'binary')
-        resolve(fileBuffer)
+        const responseType: ResponseType = "arraybuffer";
+        let opts = {
+          responseType,
+        };
+        if (axiosOptions) {
+          opts = {
+            ...opts,
+            ...axiosOptions,
+          };
+        }
+        const response = await axios.get(fileUrl, opts);
+        const fileBuffer = Buffer.from(response.data, "binary");
+        resolve(fileBuffer);
       } catch (err) {
-        return reject(err)
+        return reject(err);
       }
     }
-  })
+  });
 }
